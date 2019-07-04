98aa5613725a8ee3e5085bd257ec7fad906ec721
andreaamg22 <andreamendozag2@gmail.com>
2019-07-04 10:36:25 -0400

    Initial commit

---
+++ src/Par.java
@@ -0,0 +1,14 @@
+public class Par <E> extends Tupla<E>{
+  public E first;
+  public E second;
+  public Par (E prim, E seg){
+      this.first = prim;
+      this.second = seg;
+
+  }
+   public Par<E> Aplicar(Function<E> function){
+       return new Par<E> (function.Aplicar(this.first),function.Aplicar(this.second));
+
+   }
+
+}
---
+++ src/Tupla.java
@@ -0,0 +1,3 @@
+public abstract class Tupla < E> {
+    public abstract Tupla<E> Aplicar (Function<E> function);
+}
